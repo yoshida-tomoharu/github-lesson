f6f3b91f49ec32c104f1f846a5bda146f5fac4d6
COMNIC08 <COMNIC08@DESKTOP-QAGFUAO>
2019-06-17 16:29:12 +0900

    kanzaki push

--- github-example/src/jp/co/comnic/lesson/HardBoiledEggs/ShareFavorites/Hello.java
+++ github-example/src/jp/co/comnic/lesson/HardBoiledEggs/ShareFavorites/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!");
 		System.out.println("How are you!");
+		System.out.println("How are you!!!!!!!!!");
 	}
 }
