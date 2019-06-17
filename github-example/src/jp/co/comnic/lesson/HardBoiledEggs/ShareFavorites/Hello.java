b8c5d514d8028f81517e5f4ce1350fd17edd5ccc
Comnic14 <Comnic14@DESKTOP-SJ8CN5A>
2019-06-17 15:33:01 +0900

    Remake Java commit.

--- github-example/src/jp/co/comnic/lesson/HardBoiledEggs/ShareFavorites/Hello.java
+++ github-example/src/jp/co/comnic/lesson/HardBoiledEggs/ShareFavorites/Hello.java
@@ -3,6 +3,6 @@ package jp.co.comnic.lesson.HardBoiledEggs.ShareFavorites;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Git!");
+		System.out.println("Hello, Git!!");
 	}
 }
